db89506afadd84dca601338ba0e643ac9e226b8c
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-05-21 17:27:40 -0700

    Initial Commit

---
+++ E_Holamundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  E_Holamundo
+//
+//  Created by Aplicaciones Moviles on 21/05/15.
+//  Copyright (c) 2015 MobileLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
